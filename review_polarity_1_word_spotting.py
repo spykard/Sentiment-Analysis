2176b2fb26367940ce52ac1fa0ccc71c07b3ef77
spykard <kardakis@ceid.upatras.gr>
2018-07-25 21:06:43 +0300

    Finalize Word Spotting and Counting using Opinion Lexicon

--- review_polarity_1_word_spotting.py
+++ review_polarity_1_word_spotting.py
@@ -4,10 +4,10 @@ Sentiment Analysis Using Interesting Techniques. Bo Pang and Lillian Lee (ACL 20
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.model_selection import GridSearchCV
-from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
 from sklearn import metrics
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
