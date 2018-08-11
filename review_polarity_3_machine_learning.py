21a295b05017094bb44bb2aa6e24fb59b3669908
spykard <kardakis@ceid.upatras.gr>
2018-08-11 05:50:42 +0300

    Update Machine Learning

--- review_polarity_3_machine_learning.py
+++ review_polarity_3_machine_learning.py
@@ -114,39 +114,10 @@ np.set_printoptions(precision=10)  # Numpy Precision when Printing
 data_train, data_test, labels_train, labels_test = train_test_split(dataset.data, dataset.target, test_size=0.30, random_state=22)
 
 # Dimensionality Reduction - 4 different ways to pick the best Features 
-#   (1) ('feature_selection', SelectKBest(score_func=chi2, k=7500)),                    
-#   (2) ('feature_selection', TruncatedSVD(n_components=1000)),
+#   (1) ('feature_selection', SelectKBest(score_func=chi2, k=5000)),                    
+#   (2) ('feature_selection', TruncatedSVD(n_components=1000)),  # Has Many Issues
 #   (3) ('feature_selection', SelectFromModel(estimator=LinearSVC(), threshold='2.5*mean')),
 #   (4) ('feature_selection', SelectFromModel(estimator=LinearSVC(penalty='l1', dual=False), threshold='mean')),  # Technically L1 is better than L2
-
-
-### LET'S BUILD : MultiLayer Perceptron
-
-# Grid Search On
-pipeline = Pipeline([
-                    ('union', FeatureUnion(transformer_list=[      
-                        ('vect1', CountVectorizer(max_df=0.80, min_df=5, ngram_range=(1, 1), stop_words=stopwords_complete_lemmatized, strip_accents='unicode', tokenizer=LemmaTokenizer())),  # 1-Gram Vectorizer
-                        ('vect2', CountVectorizer(max_df=0.95, min_df=8, ngram_range=(2, 2), stop_words=None, strip_accents='unicode', tokenizer=LemmaTokenizer())),],  # 2-Gram Vectorizer
-
-                        transformer_weights={
-                            'vect1': 1.0,
-                            'vect2': 1.0,},
-                    )),
-                    ('tfidf', TfidfTransformer(use_idf=True)),
-                    #('feature_selection', SelectKBest(score_func=chi2, k=5000)),  # Dimensionality Reduction
-                    ('feature_selection', SelectFromModel(estimator=LinearSVC(), threshold='2.5*mean')),  # Dimensionality Reduction  
-                    ('clf', MLPClassifier(verbose=True)),])  
-
-parameters = {'clf__hidden_layer_sizes': [(200,)],
-              'clf__max_iter': [500],
-              'clf__solver': ['sgd'],
-              'clf__learning_rate': ['adaptive'],
-              'clf__learning_rate_init': [0.60],
-              'clf__momentum': [0.5],
-              'clf__alpha': [1e-01],}
-
-#Run_Classifier(1, 0, pipeline, parameters, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(MultiLayer Perceptron)')
-quit()
 
 
 ### LET'S BUILD : Naive Bayes
@@ -187,12 +158,11 @@ pipeline = Pipeline([ # Optimal
                             'vect1': 1.0,
                             'vect2': 1.0,},
                     )),
-
                     ('tfidf', TfidfTransformer(use_idf=True)),
-                    ('feature_selection', SelectKBest(score_func=chi2, k=7500)),  # Dimensionality Reduction                   
+                    ('feature_selection', SelectKBest(score_func=chi2, k=5000)),  # Dimensionality Reduction                   
                     ('clf', MultinomialNB()),])  
 
-Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(Naive Bayes)')
+#Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(Naive Bayes)')
 ###
 
 
@@ -235,12 +205,11 @@ pipeline = Pipeline([ # Optimal
                             'vect1': 1.0,
                             'vect2': 1.0,},
                     )),
-
                     ('tfidf', TfidfTransformer(use_idf=True)),
                     ('feature_selection', SelectFromModel(estimator=LinearSVC(), threshold='2.5*mean')),  # Dimensionality Reduction 
                     ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, max_iter=1000, tol=None, n_jobs=-1)),]) 
 
-Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(SGDC-SVM)')
+#Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(SGDC-SVM)')
 ###
 
 
@@ -283,11 +252,10 @@ pipeline = Pipeline([ # Optimal
                             'vect1': 1.0,
                             'vect2': 1.0,},
                     )),
-
                     ('tfidf', TfidfTransformer(use_idf=True)),
                     ('clf', LinearSVC(loss='hinge', penalty='l2', max_iter=1000, C=500, dual=True)),])  # dual: True for Text/High Feature Count
 
-Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(SVM)')
+#Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(SVM)')
 ###
 
 
@@ -330,10 +298,67 @@ pipeline = Pipeline([ # Optimal
                             'vect1': 1.0,
                             'vect2': 1.0,},
                     )),
-
                     ('tfidf', TfidfTransformer(use_idf=True)),
                     ('feature_selection', SelectKBest(score_func=chi2, k=5000)),  # Dimensionality Reduction
                     ('clf', LogisticRegression(penalty='l2', max_iter=1000, C=500, dual=True)),])  # dual: True for Text/High Feature Count
 
-Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(Logistic Regression)')
+#Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(Logistic Regression)')
+###
+
+
+### LET'S BUILD : MultiLayer Perceptron
+
+# Grid Search On
+pipeline = Pipeline([
+                    ('union', FeatureUnion(transformer_list=[      
+                        ('vect1', CountVectorizer()),  # 1-Grams Vectorizer
+                        ('vect2', CountVectorizer()),],  # 2-Grams Vectorizer
+                    )),
+                    ('tfidf', TfidfTransformer()),
+                    # Either of 2 Choices
+                    #('feature_selection', SelectKBest(score_func=chi2, k=5000)),  # Dimensionality Reduction
+                    ('feature_selection', SelectFromModel(estimator=LinearSVC(), threshold='2.5*mean')),  # Dimensionality Reduction  
+                    ('clf', MLPClassifier(verbose=True)),])  
+
+parameters = {'clf__hidden_layer_sizes': [(200,)],
+              'clf__max_iter': [500],
+              'clf__solver': ['sgd'],
+              'clf__learning_rate': ['adaptive'],
+              'clf__learning_rate_init': [0.60],
+              'clf__momentum': [0.50],
+              'clf__alpha': [1e-01],
+              'tfidf__use_idf': [True],
+              'union__transformer_weights': [{'vect1':1.0, 'vect2':1.0},],
+              'union__vect1__max_df': [0.80,],
+              'union__vect1__min_df': [5],  # 5 meaning 5 documents
+              'union__vect1__ngram_range': [(1, 1)],              
+              'union__vect1__stop_words': [stopwords_complete_lemmatized],
+              'union__vect1__strip_accents': ['unicode'],
+              'union__vect1__tokenizer': [LemmaTokenizer()],
+              'union__vect2__max_df': [0.95],
+              'union__vect2__min_df': [8],
+              'union__vect2__ngram_range': [(2, 2)],              
+              'union__vect2__stop_words': [None],
+              'union__vect2__strip_accents': ['unicode'],
+              'union__vect2__tokenizer': [LemmaTokenizer()],} 
+
+#Run_Classifier(1, 0, pipeline, parameters, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(MultiLayer Perceptron)')
+
+# Grid Search Off
+pipeline = Pipeline([ # Optimal
+                    ('union', FeatureUnion(transformer_list=[      
+                        ('vect1', CountVectorizer(max_df=0.80, min_df=5, ngram_range=(1, 1), stop_words=stopwords_complete_lemmatized, strip_accents='unicode', tokenizer=LemmaTokenizer())),  # 1-Gram Vectorizer
+                        ('vect2', CountVectorizer(max_df=0.95, min_df=8, ngram_range=(2, 2), stop_words=None, strip_accents='unicode', tokenizer=LemmaTokenizer())),],  # 2-Gram Vectorizer
+
+                        transformer_weights={
+                            'vect1': 1.0,
+                            'vect2': 1.0,},
+                    )),
+                    ('tfidf', TfidfTransformer(use_idf=True)),
+                    # Either of 2 Choices
+                    #('feature_selection', SelectKBest(score_func=chi2, k=5000)),  # Dimensionality Reduction
+                    ('feature_selection', SelectFromModel(estimator=LinearSVC(), threshold='2.5*mean')),  # Dimensionality Reduction  
+                    ('clf', MLPClassifier(verbose=False, hidden_layer_sizes=(200,), max_iter=500, solver='sgd', learning_rate='adaptive', learning_rate_init=0.60, momentum=0.50, alpha=1e-01)),])  
+
+Run_Classifier(0, 0, pipeline, {}, data_train, data_test, labels_train, labels_test, dataset.target_names, stopwords_complete_lemmatized, '(MultiLayer Perceptron)')
 ###
